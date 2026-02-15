84411f7296accde53dbdff9ff795683031aa3fb7
Antony Rizzitelli <upd4ting@gmail.com>
2026-02-15 19:58:35 +0100

    fix: update eslint ignore for reverted antelope.test.js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,7 @@ export default tseslint.config(
       'pnpm-lock.yaml',
       'eslint.config.mjs',
       'playground',
-      'src/test/antelope.test.ts',
+      'src/test/antelope.test.js',
     ],
   },
   eslint.configs.recommended,
